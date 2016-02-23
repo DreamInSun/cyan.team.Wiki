d40b16ef0161cd2134b0d803473c10b12b4207da
DreamInSun <yancy_chen@hotmail.com>
2016-02-23 21:19:39 +0800

    Signed-off-by: DreamInSun <yancy_chen@hotmail.com>

--- shell/docker-entrypoint.sh
+++ shell/docker-entrypoint.sh
@@ -5,6 +5,17 @@ echo ========== Configuration ==========
 
 export DOKUWIKI_ROOT=$APACHE_DOC
 
+mkdir -p $DOKUWIKI_ROOT/data/pages
+mkdir -p $DOKUWIKI_ROOT/data/attic 
+mkdir -p $DOKUWIKI_ROOT/data/media 
+mkdir -p $DOKUWIKI_ROOT/data/media_attic 
+mkdir -p $DOKUWIKI_ROOT/data/media_meta 
+mkdir -p $DOKUWIKI_ROOT/data/meta 
+mkdir -p $DOKUWIKI_ROOT/data/cache
+mkdir -p $DOKUWIKI_ROOT/data/locks 
+mkdir -p $DOKUWIKI_ROOT/data/index 
+mkdir -p $DOKUWIKI_ROOT/data/tmp 
+
 chmod -R 777 $DOKUWIKI_ROOT/conf
 chmod -R 777 $DOKUWIKI_ROOT/data
 
