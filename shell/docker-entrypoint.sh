c3cf328d8095c7a6a13cba38a52cd31b87bacee3
DreamInSun <yancy_chen@hotmail.com>
2016-02-24 00:47:01 +0800

    Signed-off-by: DreamInSun <yancy_chen@hotmail.com>

--- shell/docker-entrypoint.sh
+++ shell/docker-entrypoint.sh
@@ -3,12 +3,8 @@
 echo ========== Configuration ==========
 
 if [ ! -d "$DOKUWIKI_ROOT/data/pages"]; then 
+  echo Initialize data
   cp -rf $DOKUWIKI_ROOT/data_init/* $DOKUWIKI_ROOT/data
-fi
-
-if [ ! -d "/backup/conf"]; then 
-  mkdir -p /backup/conf/
-  cp -rf $DOKUWIKI_ROOT/conf_init/* /backup/conf/
 fi
 
 chmod -R 777 $DOKUWIKI_ROOT/conf
