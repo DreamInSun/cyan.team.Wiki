9ee5450bd2bfb94f11448ee0e064fcb504461b73
DreamInSun <yancy_chen@hotmail.com>
2016-02-23 21:14:45 +0800

    Signed-off-by: DreamInSun <yancy_chen@hotmail.com>

--- shell/docker-entrypoint.sh
+++ shell/docker-entrypoint.sh
@@ -3,5 +3,10 @@
 echo ========== Configuration ==========
 #php config.php
 
+export DOKUWIKI_ROOT=$APACHE_DOC
+
+chmod -R 777 $DOKUWIKI_ROOT/conf
+chmod -R 777 $DOKUWIKI_ROOT/data
+
 echo ========== Start Httpd ==========
 apache2-foreground
